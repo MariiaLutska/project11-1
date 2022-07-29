4263a310541e4cef7bc3e3c0787dfada507d5160
MariiaLutska <myklush@ukr.net>
2022-07-29 12:09:59 +0300

    js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,28 @@ import './js/theme';
 // import './js/spinner';
 import './js/scrollTop';
 
-//import './js/openModal';
-//import './js/modalAppearanceToggle';
+import './js/openModal';
+import './js/fetch';
+import './js/fetchFilm';
+import './js/fetchPopular';
+import './js/footer-modal-close';
+import './js/genre';
+import './js/lib';
+import './js/markup-pages';
+import './js/markUp';
+import './js/markupCard';
+import './js/markupModalCard';
+import './js/modalClose';
+import './js/modalTrailer';
+import './js/openModal';
+import './js/ref-pagin';
+import './js/ref';
+
+
+
+
+
+
 import './js/modalClose';
 import './js/modalTrailer';
 //import './js/localStorageAPI';
